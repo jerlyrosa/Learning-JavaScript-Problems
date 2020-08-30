11fcd0342ea553a76e16986865fe81638942f2a1
JerlyM05 <correo>
2020-08-29 20:49:47 -0400

    Problema 8/100 Challenger

---
+++ Problems/problem-8.js
@@ -0,0 +1,9 @@
+// #100DaysOfCode 8/100
+
+// Cree una función que tome una matriz de cadenas y números, y filtre la matriz para que devuelva solo 
+// una matriz de enteros.
+// filterArray([1, 2, 3, "a", "b", 4]) ➞ [1, 2, 3, 4]
+
+const filterArray =(arr)  => {return  arr.filter(arr => Number.isInteger(arr));}
+
+filterArray([1, 2, 3, "a", "b", 4]);
