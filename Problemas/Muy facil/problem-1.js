cae7bb8288c26252aa2f782c24e30462b0fe893a
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-08-30 20:52:15 -0400

    Corrigiendo errores

--- Problemas/Muy facil/problem-1.js
+++
@@ -1,6 +0,0 @@
-// Todo lo que tiene que hacer es escribir return "hello edabit.com"entre las llaves { }y luego hacer clic
-//  en el botón Verificar . Si lo hizo correctamente, el botón se volverá rojo y dirá ENVIAR FINAL . Haga 
-//  clic en él y vea qué sucede.
-
-// Nota
-// La cadena devuelta debe estar en minúsculas .
