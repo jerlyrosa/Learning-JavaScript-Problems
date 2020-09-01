679581fb6e38ac67d72ff95095115e88b12fd20d
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-08-31 20:53:41 -0400

    Resovilendo 1 problema de la categoria facil

---
+++ Problemas/Facil/problem-8.js
@@ -0,0 +1,16 @@
+// Invertir una matriz
+// Escribe una función para invertir una matriz.
+
+
+// Ejemplos
+
+// reverse([1, 2, 3, 4]) ➞ [4, 3, 2, 1]
+// reverse([9, 9, 2, 3, 4]) ➞ [4, 3, 2, 9, 9]
+// reverse([]) ➞ []
+
+// Notas
+// No olvide returnel resultado
+
+const reverse = (arr) => {return arr.reverse()} //return [4, 3, 2, 9, 9]
+// metodo reverse() devulve el array en su forma invertid.
+reverse([9, 9, 2, 3, 4]) 
