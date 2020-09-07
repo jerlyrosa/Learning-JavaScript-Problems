8157305cdd915e3b521a8b46ef239df965adc441
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-09-06 20:31:43 -0400

    Resolviendo un prblema muy facil

---
+++ Problemas/MuyFacil/problem-15.js
@@ -0,0 +1,14 @@
+// ¿Son iguales los números?
+// Cree una función que devuelva truecuando num1sea ​​igual a num2; de lo contrario, vuelva false.
+
+// Ejemplos
+// isSameNum(4, 8) ➞ false
+
+// isSameNum(2, 2) ➞  true
+
+// isSameNum(2, "2") ➞ false
+// Notas
+// No olvide returnel resultado.
+
+const isSameNum = (num1, num2) => {return num1 === num2 ? true : false;}//return  false
+isSameNum(2, "2");//Paramentro que resive la funcion
