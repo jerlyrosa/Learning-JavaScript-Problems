55c9960e7f28f544aa93b835a7e93d57e41f5597
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-09-06 20:21:55 -0400

    Resolviendo un prblema muy facil

---
+++ Problemas/MuyFacil/problem-14.js
@@ -0,0 +1,14 @@
+// ¿Menos de 100?
+// Dados dos números, devuelva truesi la suma de ambos números es menor que 100. De lo contrario,
+//  devuelva false.
+
+// Ejemplos
+// lessThan100(22, 15) ➞ true
+// // 22 + 15 = 37
+
+// lessThan100(83, 34) ➞ false
+// // 83 + 34 = 117
+
+// lessThan100(3, 77) ➞ true
+const  lessThan100 =(a, b)=> { return a+b < 100 ? true : false;}// return  false
+lessThan100(83, 34);//Paramentro que resive la funcion
