3fd463e004b420ff16b8ec28bd714372c14e4216
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-09-28 20:14:30 -0400

    Resolviendo un prblema facil

---
+++ Problemas/Principiante/MuyFacil/problem-18.js
@@ -0,0 +1,14 @@
+// Recursión: longitud de una cadena
+
+// Escribe una función que devuelva la longitud de una cadena. Haz que tu función sea recursiva.
+
+// Ejemplos
+
+// length("apple") ➞ 5
+// length("make") ➞ 4
+// length("a") ➞ 1
+// length("") ➞ 0
+
+
+const lengthString = (str) =>  str.length;
+console.log(lengthString("make"))
