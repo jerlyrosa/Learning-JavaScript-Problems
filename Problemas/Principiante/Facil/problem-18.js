4b1489ec21550e82a28695b59ff38279d6b30f1c
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-10-10 16:29:47 -0400

    Resolviendo un problema Facil

---
+++ Problemas/Principiante/Facil/problem-18.js
@@ -0,0 +1,8 @@
+// Programa una función que invierta las palabras de una cadena de texto, pe.
+//  miFuncion("Hola Mundo") devolverá "odnuM aloH".
+
+const palindrome =(str)=>console.log( str.split('').reverse().join(''));
+palindrome('Hola Mundo');
+
+
+
