77c4ee0fda3c708c36c14a0df9ff69bda345a19e
JerlyM05 <jerlymigueldelarosah@gmail.com>
2020-10-28 21:11:00 -0400

    Resolviendo un problema MuyFacil

---
+++ Problemas/Principiante/MuyFacil/problem-22.js
@@ -0,0 +1,6 @@
+// 9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+const randomNumber =(max,min) => Math.round(Math.random() * (max - min) + min);
+
+const resul = randomNumber(501,600);
+console.log(resul);
